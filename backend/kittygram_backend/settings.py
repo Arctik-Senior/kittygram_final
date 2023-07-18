d3a60bece5ce3c80fe19f7d006069a59086bca26
Кирилл Хорошилов <khoroshilovkirill@gmail.com>
2023-07-19 00:10:16 +0300

    fix settings 1.2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -13,7 +13,7 @@ DEBUG = bool(os.getenv('DEBUG', 'False').lower())
 
 # сорри за комент, знаю что нужно ALLOWED_HOSTS держать в .env, но на серве бекенд никак не хочет работать
 # коменты потом удалю
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(', ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(',')
 # ALLOWED_HOSTS = ['meowkittygram.ddns.net', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
